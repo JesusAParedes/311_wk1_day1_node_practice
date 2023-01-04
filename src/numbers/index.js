398bd72fd10820a32d131512592f787f8cf52cec
JesusAParedes <paredesjesus93@gmail.com>
2023-01-03 19:37:20 -0600

    date tests passing and node modules installed

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,11 +1,11 @@
 const isEven = (num) => {
   // write code for numbers.isEven
-
+return num % 2 === 0;
 }
 
 const sum = (arr) => {
   // write code for numbers.sum
-
+arr.reduce(arr, total => (arr), total)
 }
 
 const comboSum = (arr, sum) => {
