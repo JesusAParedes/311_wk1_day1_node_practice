854e685c4949a9370fac3ed808f9caf6a9fec293
JesusAParedes <paredesjesus93@gmail.com>
2023-01-03 20:40:24 -0600

    numbers tests passing and 2/3 strings passing

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -5,12 +5,24 @@ return num % 2 === 0;
 
 const sum = (arr) => {
   // write code for numbers.sum
-arr.reduce(arr, total => (arr), total)
+  // let sum = arr.reduce((arr, total) => {(arr), total});
+  let sum = 0;
+  for(i = 0; i <arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 }
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
 
+for(i=0; i<arr.length; i++) {
+  for(j=0; j<i-1; j++) {
+    if((arr[i] + arr[j]) === sum) {
+    }
+    return false;
+  }
+}
 }
 
 module.exports = {
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,15 +1,23 @@
 const split = (str, delim) => {
   // write code for strings.split
-
+  return str.split(delim);
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
-
+  // let pairs = [];
+  // for(i=0; i<str.length; str+2) {
+  //   let pair = str.split();
+  //   pairs.push(pair);
+  // }
+  // return pairs;
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
+  let arr = str.split('');
+  arr.reverse();
+  return arr.join('');
 
 }
 
